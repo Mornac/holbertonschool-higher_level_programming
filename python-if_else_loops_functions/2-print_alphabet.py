c3f35a39617555d08687086b809121a1f4d740a0
Mornac <imornac@gmail.com>
2025-05-08 10:20:53 +0200

    Fix the variable of the for

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-lowercase_alphabet = ''.join(chr(i) for i in range(ord('a'), ord('z') + 1))
-print(lowercase_alphabet)
+for letter in range(ord('a'), ord('z') + 1):
+    print(chr(letter), end=' ')
