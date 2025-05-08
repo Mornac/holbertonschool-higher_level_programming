9ab67372e7de10bf11102fd5fa3d1b572479d933
Mornac <imornac@gmail.com>
2025-05-08 17:26:18 +0200

    delete space after argument line 10

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 import sys
 
-def main():
+if __name__ == "__main__":
     argv = sys.argv[1:]
-    if len(argv) == 0:
+    argc = len(argv)
+    if argc == 0:
         print("0 arguments.")
-    elif len(argv) == 1:
-        print("1 argument :")
+    elif argc == 1:
+        print("1 argument:")
         print("1: {}".format(argv[0]))
     else:
         print("{} arguments:".format(len(argv)))
         for i, argv in enumerate(argv, start=1):
             print("{}: {}".format(i, argv))
-
-if __name__ == "__main__":
-    main()
