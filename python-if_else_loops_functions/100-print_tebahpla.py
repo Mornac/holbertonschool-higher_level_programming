76a585998fb51eb02fdbbe3bab98368ad112ab2c
Mornac <imornac@gmail.com>
2025-05-09 09:50:13 +0200

    fix a fault in the shebang

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python3
-#!/usr/bin/env python3
-result = []
+#!/usr/bin/python3
+result = ""
 for i in range(25, -1, -1):
     char = chr(97 + i)
     if i % 2 == 0:
-        result.append(char.upper())
-else:
-    result.append(char)
-print("".join(result), end="")
+        result += char.upper()
+    else:
+        result += char
+print(result, end="")
