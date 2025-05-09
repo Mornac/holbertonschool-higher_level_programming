f8036b9631a85251b1f628d48fd1c9dbf5becaf5
Mornac <imornac@gmail.com>
2025-05-09 12:10:50 +0200

    fix: replace exit by sys.exit

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     argv = sys.argv
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
     a = int(argv[1])
     operator = argv[2]
     b = int(argv[3])
@@ -20,4 +20,4 @@ if __name__ == "__main__":
         print("{} {} {} = {}".format(a, operator, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
