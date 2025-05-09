db6168a24a72b98066f3240cf6a5d779728fa1b7
Mornac <imornac@gmail.com>
2025-05-09 09:33:55 +0200

    prints the ASCII alphabet, in reverse order, alternating lowercase and uppercase

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+result = ""
+for i in range(25, -1, -1):
+    char = chr(97 + i)
+    if i % 2 == 0:
+        result += char.upper()
+    else:
+        result += char
+
+print(result)
