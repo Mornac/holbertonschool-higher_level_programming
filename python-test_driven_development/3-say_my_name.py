33137837a48c7cfcd5665f7439c40cf81054cfe8
Mornac <imornac@gmail.com>
2025-05-16 16:27:54 +0200

    prints My name is first name last name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+The module provides a function that prints my name.
+
+first_name and last_name must be strings.
+Each row of the matrix must have the same size.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+
+    """
+    Prints first_name and last_name.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
