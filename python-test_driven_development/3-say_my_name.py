36b9975979e8c75fa581370e090d1be1470f6567
Mornac <imornac@gmail.com>
2025-05-16 20:17:02 +0200

    fix: del line before print

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -8,7 +8,6 @@ The module provides a function that prints a sentence.
 def say_my_name(first_name, last_name=""):
     """
     Prints "My name is <first_name> <last_name>".
-    
     Parameters:
         first_name (str): The first name.
         last_name (str, optional): The last name. Defaults to "".
@@ -20,5 +19,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
     print("My name is {} {}".format(first_name, last_name))
