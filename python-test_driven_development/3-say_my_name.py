30467635c9056e5cbad5e5ab81bb320e56ea16f1
Mornac <imornac@gmail.com>
2025-05-16 20:13:57 +0200

    Add: tests in None cases

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -2,20 +2,20 @@
 
 """
 The module provides a function that prints a sentence.
-
-first_name and last_name must be strings.
 """
 
 
 def say_my_name(first_name, last_name=""):
     """
-    The function prints 'My name is <first_name> <last_name>.
-
+    Prints "My name is <first_name> <last_name>".
+    
     Parameters:
-    first_name (str): first name
-    last_name (str): last name
-    """
+        first_name (str): The first name.
+        last_name (str, optional): The last name. Defaults to "".
 
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
