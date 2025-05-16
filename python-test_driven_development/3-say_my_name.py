e0928619999cda6d1b60951ffce4826d3a4f97f8
Mornac <imornac@gmail.com>
2025-05-16 19:10:16 +0200

    fix: suppr line between def and his doc

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-The module provides a function that prints my name.
+The module provides a function that prints a sentence.
 
 first_name and last_name must be strings.
 Each row of the matrix must have the same size.
@@ -9,13 +9,13 @@ Each row of the matrix must have the same size.
 
 
 def say_my_name(first_name, last_name=""):
-
     """
-    The function say my name.
+    The function prints my name.
     """
 
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
+
     print("My name is {} {}".format(first_name, last_name))
