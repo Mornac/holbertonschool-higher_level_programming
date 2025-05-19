e90635555524f11c3c548d265b7d0718b6fdf530
Mornac <imornac@gmail.com>
2025-05-19 17:34:48 +0200

    fix: delete function after def init like in task 0

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -15,12 +15,8 @@ class Square:
         Args:
             size (int): The size of the square by default.
         """
-        self.__size = size
-        """
-        The private size of the square.
-        """
-        size = int(size)
-        if size != int(size):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
