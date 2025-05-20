30c5e3539c4cf55fa08efc48af29f99b7509901b
Mornac <imornac@gmail.com>
2025-05-20 11:59:09 +0200

    fix: change width and heigh by self public

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -16,8 +16,8 @@ class Square:
             size (int): The size of the square by default.
             position (tuple): a tuple of 2 positive integers.
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
