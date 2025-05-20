702287113b73e813d044aa9517d95cb4fae72370
Mornac <imornac@gmail.com>
2025-05-20 08:26:38 +0200

    Write an empty class Rectangle that defines a rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module defines a class Rectangle.
+"""
+
+
+class Rectangle:
+    """
+    This class represents a rectangle.
+    """
+    def __init__(self, width=0, height=0):
+        """
+        Initialize the rectangle with a given width and height.
+
+        Args:
+            width (int): The width of the rectangle by default.
+            height (int): The height of the rectangle by default.
+        """
