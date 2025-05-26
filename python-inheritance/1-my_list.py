c77476bc8fb54de5e7f0416d02f85f275740529d
Mornac <imornac@gmail.com>
2025-05-26 17:41:43 +0200

    fix error file 1 cause too much written info

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -8,22 +8,6 @@ class MyList(list):
     """
     Class that inherits from the built-in list class
     """
-    def __init__(self, list_instance=None):
-        """
-        Initializes the MyList instance
-        Args:
-        list_instance (list): The list of integers.
-        """
-        if list_instance is None:
-            list_instance = []
-        elif isinstance(list_instance, int):
-            list_instance = [list_instance]
-        elif not isinstance(list_instance, list):
-            raise TypeError("list_instance must be a list of integers")
-        if not all(isinstance(i, int) for i in list_instance):
-            raise TypeError("list_instance must be a list of integers")
-        super().__init__(list_instance)
-
     def print_sorted(self):
         """
         Prints the list in ascending order
