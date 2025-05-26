ea03180b38dd3eaacf609e4cc2eac2bf98f91d8d
Mornac <imornac@gmail.com>
2025-05-26 12:49:31 +0200

    fix: add the printed list in order

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,37 +4,28 @@ Module that defines a class MyList
 """
 
 
-class list(list):
-    """
-    Class that inherits from the built-in list class
-    """
-    def __init__(self):
-        """
-        Initializes the MyList instance
-        """
-        super().__init__()
-
-    def list(self, list_instance):
-        """
-        Returns the list instance
-        """
-        list_instance = int(list_instance)
-        if isinstance(list_instance, list):
-            return list_instance
-        else:
-            raise TypeError("list_instance must be a list of integers")
-
-
 class MyList(list):
     """
     Class that inherits from the built-in list class
     """
-    def __init__(self, list_instance):
+    def __init__(self, list_instance=None):
         """
         Initializes the MyList instance
+        Args:
+        list_instance (list): The list of integers.
+        Raises:
+        TypeError: If list_instance is not a list of integers.
         """
+        if list_instance is None:
+            list_instance = []
         if not isinstance(list_instance, list):
             raise TypeError("list_instance must be a list")
         if not all(isinstance(i, int) for i in list_instance):
             raise TypeError("list_instance must be a list of integers")
         super().__init__(list_instance)
+    
+    def print_sorted(self):
+        """
+        Prints the list in ascending order
+        """
+        print(sorted(self))
