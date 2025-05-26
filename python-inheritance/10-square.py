4216d281540bc5d0877f44907a51ebd42420d5e4
Mornac <imornac@gmail.com>
2025-05-26 18:44:39 +0200

    fix: add class rectangle

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -32,9 +32,46 @@ class BaseGeometry:
             raise ValueError("{} must be greater than 0".format(name))
 
 
-class Square(BaseGeometry):
+class Rectangle(BaseGeometry):
     """
-    Square class that inherits from BaseGeometry.
+    Rectangle class that inherits from BaseGeometry.
+    It validates the width and height and provides an area method.
+    """
+    def __init__(self, width, height):
+        """
+        Initializes the Rectangle class with width and height.
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        Raises:
+            TypeError: If width or height is not an integer.
+            ValueError: If width or height is less than or equal to 0.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """
+        Calculates the area of the rectangle.
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Returns a string representation of the rectangle.
+        Returns:
+            str: A string in the format "[Rectangle] <width>/<height>".
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from Rectangle.
     It validates the size and provides an area method.
     """
     def __init__(self, size):
