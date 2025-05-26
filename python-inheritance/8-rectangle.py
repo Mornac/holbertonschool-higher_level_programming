ff173435f451f69646895eca83aeb6da89ac820b
Mornac <imornac@gmail.com>
2025-05-26 17:26:35 +0200

    fix: correct the class bg of file 7 and impact the file 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -29,3 +29,25 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class that inherits from BaseGeometry.
+    It validates the width and height and provides an area method.
+    """
+    def __init__(self, width, height):
+        """
+        Initializes the Rectangle class with width and height.
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        Raises:
+            TypeError: If width or height must be an integer.
+            AttributeError: If 'Rectangle' object has no attribute
+                'width' or 'height'.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
