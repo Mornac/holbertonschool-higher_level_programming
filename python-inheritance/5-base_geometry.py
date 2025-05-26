20b71eece718720a33572ad9e6032c9db4eda4c5
Mornac <imornac@gmail.com>
2025-05-26 15:00:37 +0200

    Write an empty class BaseGeometry

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -8,19 +8,4 @@ class BaseGeometry:
     """
     class with a method to check if an object is an instance of the class.
     """
-
-    def __init__(self):
-        """Initialize the BaseGeometry instance."""
-        pass
-
-    def is_instance(self, obj):
-        """
-        Check if obj is exactly an instance of BaseGeometry.
-
-        Args:
-            obj: The object to check.
-
-        Returns:
-            True if obj is an instance of BaseGeometry, False otherwise.
-        """
-        return isinstance(obj, BaseGeometry)
+    pass
