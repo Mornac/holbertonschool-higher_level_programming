8ac77894bed5374cb11e1a0c1c47fec0084232b2
Mornac <imornac@gmail.com>
2025-05-27 16:12:02 +0200

    implemente the prints

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -3,19 +3,19 @@
 class SwimMixin:
     def swim(self):
         self.swim = True
-        print("The creature swims!")
+    print("The creature swims!")
 
 
 class FlyMixin:
     def fly(self):
         self.fly = True
-        print("The creature flies!")
+    print("The creature flies!")
 
 
 class Dragon(SwimMixin, FlyMixin):
     def roar(self):
         self.roar = True
-        print("The dragon roars!")
+    print("The dragon roars!")
 
     def draco(self):
         self.fly()
