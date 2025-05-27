0bc8536abb8824f987a7e26a135069d6f0488843
Mornac <imornac@gmail.com>
2025-05-27 15:45:45 +0200

    Create Mixins and implemente Dragon

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+class SwimMixin:
+    def swim(self):
+        self.swim = True
+        print("The creature swims!")
+
+
+class FlyMixin:
+    def fly(self):
+        self.fly = True
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    def roar(self):
+        self.roar = True
+        print("The dragon roars!")
+
+    def draco(self):
+        self.fly()
+        self.swim()
+        self.roar()
