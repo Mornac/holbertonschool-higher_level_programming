80ee8af85bb745729fba74bc31b11a71f581d87d
Mornac <imornac@gmail.com>
2025-05-27 15:07:58 +0200

    fix: add the import instead of the class BG

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,32 +4,7 @@ Function that checks if an object is exactly an instance of a specified class.
 """
 
 
-class BaseGeometry:
-    """
-    class with a method to check if an object is an instance of the class.
-    """
-    pass
-
-    def area(self):
-        """
-        Raises an exception if the area method is not implemented.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validates that value is an integer greater than 0.
-        Args:
-            name (str): is always a string.
-            value (int): The value to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than or equal to 0.
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
