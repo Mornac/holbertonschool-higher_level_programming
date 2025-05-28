80c5e9b0373c20c6a5aac83a06e8ecc13db6f3aa
Mornac <imornac@gmail.com>
2025-05-28 13:18:23 +0200

    fix: delete too much comments

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -17,12 +17,6 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """
         Validates that value is an integer greater than 0.
-        Args:
-            name: is always a string.
-            value: The value to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than or equal to 0.
         """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
