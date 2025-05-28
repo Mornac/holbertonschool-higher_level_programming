33a9e01f2141b653c985c3e094ad7777aa56c47e
Mornac <imornac@gmail.com>
2025-05-28 10:32:27 +0200

    fix errors between size and width

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -74,18 +74,19 @@ class Square(Rectangle):
     It validates the size and provides an area method.
     """
 
-    def __init__(self, width):
+    def __init__(self, size):
         """
         Initializes the Square class with size.
         Args:
             size (int): The size of the square.
         Raises:
-            TypeError: If size must be an integer.
+            TypeError: size must be an integer.
             AttributeError:
                 If 'Square' object has no attribute 'size'.
         """
-        self.integer_validator("width", width)
-        self.__width = width
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """
@@ -93,12 +94,11 @@ class Square(Rectangle):
         Returns:
             int: The area of the square.
         """
-        return self.__width * self.__width
-    
+        return self.__size * self.__size
+
     def __str__(self):
         """
-        Returns a string representation of the square.
         Returns:
             str: A string in the format "[Rectangle] <size>/<size>".
         """
-        return "[Rectangle] {}/{}".format(self.__width, self.__width)
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
