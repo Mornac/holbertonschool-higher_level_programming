577554218bd6422a578f00dc8b1f7a4aeb116689
Mornac <imornac@gmail.com>
2025-05-28 11:11:02 +0200

    fix delete def draco

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -2,22 +2,14 @@
 
 class SwimMixin:
     def swim(self):
-        self.swim = True
-    print("The creature swims!")
+        print("The creature swims!")
 
 
 class FlyMixin:
     def fly(self):
-        self.fly = True
-    print("The creature flies!")
+        print("The creature flies!")
 
 
 class Dragon(SwimMixin, FlyMixin):
     def roar(self):
-        self.roar = True
-    print("The dragon roars!")
-
-    def draco(self):
-        Dragon.fly()
-        Dragon.swim()
-        Dragon.roar()
+        print("The dragon roars!")
