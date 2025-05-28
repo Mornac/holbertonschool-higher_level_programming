d0a9bcca409cf149d3248b8af21eb4c86da2098d
Mornac <imornac@gmail.com>
2025-05-28 13:10:48 +0200

    fix error in format size

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -12,18 +12,18 @@ class Square(Rectangle):
     Square class that inherits from Rectangle.
     It validates the size and provides an area method.
     """
-    def __init__(self, width):
+    def __init__(self, size):
         """
         Initializes the Square class with size.
         Args:
             size (int): The size of the square.
         Raises:
-            TypeError: If size must be an integer.
+            TypeError: size must be an integer.
             AttributeError:
                 If 'Square' object has no attribute 'size'.
         """
-        self.integer_validator("width", width)
-        self.__width = width
+        self.integer_validator("size", size)
+        self.__size = size
 
     def area(self):
         """
@@ -31,12 +31,12 @@ class Square(Rectangle):
         Returns:
             int: The area of the square.
         """
-        return self.__width * self.__width
+        return self.__size * self.__size
 
     def __str__(self):
         """
         Returns a string representation of the square.
         Returns:
-            str: A string in the format "[Square] <width>/<height>".
+            str: A string in the format "[Square] <size>/<size>".
         """
-        return "[Square] {}/{}".format(self.__width, self.__width)
+        return "[Square] {}/{}".format(self.__size, self.__size)
