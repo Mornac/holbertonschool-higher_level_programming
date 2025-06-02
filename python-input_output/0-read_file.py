2835715fb6e4ae52e03fb26dfe1b5a35899aefae
Mornac <imornac@gmail.com>
2025-06-02 09:42:53 +0200

    function that reads a text file (UTF8) and prints it to stdout

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module that reads a text file
+"""
+
+
+def read_file(filename=""):
+    """
+    Function that reads a text file and prints it to stdout
+    Args:
+        filename (str): name of the file to read
+    """
+    with open("my_file_0.txt", "r") as file:
+        content = file.read()
+        print(content, end="")
