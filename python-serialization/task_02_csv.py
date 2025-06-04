2eee6d59c25c848dab0fcaae426ca6a09dbd3608
Mornac <imornac@gmail.com>
2025-06-04 13:35:45 +0200

    create main02csv

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -7,34 +7,16 @@ import csv
 import json
 
 
-def convert_csv_to_json(csv, filename):
+def convert_csv_to_json(csv_file):
     """
     Convert CSV file to JSON format.
 
     :param CSV filename: The name of the CSV file to convert.
     :return: JSON data as a string.
     """
-    data = []
-    with open(filename, 'w', newline='') as csv_file:
-        csv_writer = csv.DictWriter(csv_file)
-        try:
-            csv_writer.writeheader()
-        except csv.Error:
-            # If the CSV file is empty, we cannot write a header
-            pass
-        for row in csv_writer:
-            data.append(row)
-            return True
-
-    with open(filename, 'r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        try:
-            csv_reader.fieldnames
-        except csv.Error:
-            # If the CSV file is empty, we cannot read it
-            return json.dumps(data, indent=4)
-        for row in csv_reader:
-            data.append(row)
-            return True
-
-    return json.dumps(data, indent=4)
+    with open(csv_file, 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        data = list(reader)
+        
+    with open('data.json', 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, indent=4)
