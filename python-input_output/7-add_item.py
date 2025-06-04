3e17ae0759865c0c0a9bc7cf6006424f9d06d56f
Mornac <imornac@gmail.com>
2025-06-04 08:08:19 +0200

    fix: delete line between import and json files

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,24 +8,12 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def main():
-    """
-    Adds all arguments to a Python list and saves them to a file.
-    """
-    if len(sys.argv) < 2:
-        SystemExit()
+filename = "add_item.json"
 
-    filename = "add_item.json"
-    try:
-        my_list = load_from_json_file(filename)
-    except FileNotFoundError:
-        my_list = []
+try:
+    my_list = load_from_json_file(filename)
+except Exception:
+    my_list = []
 
-    for i in range(1, len(sys.argv)):
-        my_list.append(sys.argv[i])
-
+    my_list.extend(sys.argv[1:])
     save_to_json_file(my_list, filename)
-
-
-if __name__ == "__main__":
-    main()
