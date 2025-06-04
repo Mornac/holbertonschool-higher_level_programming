cc9cd5c79de16934b0f1e9662c7fb7934667638d
Mornac <imornac@gmail.com>
2025-06-04 08:03:10 +0200

    Add import sys in the code

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,23 +3,29 @@
 Module that adds all arguments to a Python list and saves them to a file.
 """
 import json
-
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def add_item(args, filename='add_item.json'):
+def main():
     """
     Adds all arguments to a Python list and saves them to a file.
-    Args:
-        args: The list of arguments to add.
-        filename: The name of the file to save the list to.
     """
-    if not isinstance(filename, str):
-        filename = 'add_item.json'
+    if len(sys.argv) < 2:
+        SystemExit()
+
+    filename = "add_item.json"
     try:
-        items = load_from_json_file(filename)
+        my_list = load_from_json_file(filename)
     except FileNotFoundError:
-        items = []
-    items.extend(args)
-    save_to_json_file(items, filename)
+        my_list = []
+
+    for i in range(1, len(sys.argv)):
+        my_list.append(sys.argv[i])
+
+    save_to_json_file(my_list, filename)
+
+
+if __name__ == "__main__":
+    main()
