a56a7bace4dc2d30399ecb1b498c86b8e8f8929b
Mornac <imornac@gmail.com>
2025-06-04 09:31:54 +0200

    fix trailing whitespace  line 29 pycodestyle

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -11,6 +11,10 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """
         Initialize the student with first_name, last_name and age.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
@@ -20,9 +24,15 @@ class Student:
         """
         Retrieves a dict representation of a student instance
             and writes it to a file.
+        Args:
+            attrs (list, optional): A list of attributes to include in the
+                dictionary representation.
+            If None, all attributes are included.
+        Returns:
+            dict: A dictionary representation of the student instance.
         """
         if attrs is None:
-             return self.__dict__
+            return self.__dict__
         else:
             return {key: self.__dict__[key] for key in attrs
                     if key in self.__dict__}
