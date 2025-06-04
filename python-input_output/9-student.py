aac91f854d8c8a11e9e7b25f58b433c1bea44589
Mornac <imornac@gmail.com>
2025-06-04 09:08:25 +0200

    fix: delet the import

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -8,7 +8,6 @@ class Student:
     """
     A class representing a student with methods to read and write data.
     """
-
     def __init__(self, first_name, last_name, age):
         """
         Initialize the student with first_name, last_name and age.
@@ -17,19 +16,8 @@ class Student:
         self.last_name = last_name
         self.age = age
     
-    def to_json(self, filename):
+    def to_json(self):
         """
         Retrieves a dict representation of a student instance and writes it to a file.
         """
-        with open(filename, 'w') as file:
-            file.write(f"{self.first_name},{self.last_name},{self.age}\n")
-
-    @classmethod
-    def read_from_file(cls, filename):
-        """
-        Read student data from a file and return an instance of student.
-        """
-        with open(filename, 'r') as file:
-            line = file.readline().strip()
-            first_name, last_name, age = line.split(',')
-            return (first_name, last_name, int(age))
+        return self.__dict__
