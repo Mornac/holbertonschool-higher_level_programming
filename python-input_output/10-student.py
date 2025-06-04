1e5d2aa67ec4b1b010f09bd0d83d8dc325549298
Mornac <imornac@gmail.com>
2025-06-04 09:27:16 +0200

    delete import and add class student from file 9

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -2,18 +2,27 @@
 """
 Module that defines a Student class
 """
-Student = __import__('9-student').Student
 
 
-def to_json(self, attrs=None):
+class Student:
     """
-    Retrieves a dict representation of a student instance
-        and writes it to a file.
+    A class representing a student with methods to read and write data.
     """
-    if attrs is None:
-        return self.__dict__
-    else:
-        return {k: v for k, v in self.__dict__.items() if k in attrs}
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize the student with first_name, last_name and age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-
-Student.to_json = to_json
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dict representation of a student instance
+            and writes it to a file.
+        """
+        if attrs is None:
+             return self.__dict__
+        else:
+            return {key: self.__dict__[key] for key in attrs
+                    if key in self.__dict__}
