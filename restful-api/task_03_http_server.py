20df5be87eb11f45739ca08c88b2fece60a29668
Mornac <imornac@gmail.com>
2025-06-10 11:41:48 +0200

    fix line return after write

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -18,23 +18,23 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-Type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'Hello, this is a simple API!\n')
+            self.wfile.write(b'Hello, this is a simple API!')
         elif self.path == '/data':
             self.send_response(200)
             self.send_header('Content-Type', 'application/json')
             self.end_headers()
-            response_data = {"message": "This is some sample data."}
+            response_data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(response_data).encode('utf-8'))
         elif self.path == '/status':
             self.send_response(200)
             self.send_header('Content-Type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'OK.\n')
+            self.wfile.write(b'OK')
         else:
             self.send_response(404)
             self.send_header('Content-Type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'Endpoint not found.\n')
+            self.wfile.write(b'Endpoint not found')
 
 
 if __name__ == '__main__':
