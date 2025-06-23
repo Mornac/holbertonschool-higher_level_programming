1d77aaa23f1c39a80d617c54c818658b06fc6872
Mornac <imornac@gmail.com>
2025-06-23 16:07:54 +0200

    lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- 15 Number by score
+-- Script that lists the number of records with the same score
+-- in the second_table table of database hbtn_0c_0 in my MySQL server
+SELECT score, count(*) as 'number' FROM second_table GROUP BY score;
