7cac2bad9fe197264c2af242aae3aa19746e4388
Mornac <imornac@gmail.com>
2025-06-23 16:52:02 +0200

    converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- 17 Go to UTF8
+-- Script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+-- in MySQL server
+ALTER DATABASE hbtn_0c_0 CHARACTER SET UTF8mb4 COLLATE utf8mb4_unicode_ci;
