27540b37c34d187ab0c0e67c162c9fb2533d8a52
Mornac <imornac@gmail.com>
2025-06-23 16:10:41 +0200

    add order by score desc

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- 15 Number by score
 -- Script that lists the number of records with the same score
 -- in the second_table table of database hbtn_0c_0 in my MySQL server
-SELECT score, count(*) as 'number' FROM second_table GROUP BY score;
+SELECT score, count(*) as 'number' FROM second_table GROUP BY score ORDER BY score DESC;
