f7c202933854b28c0192a811b355ce7c4aa32528
Mornac <imornac@gmail.com>
2025-06-23 15:53:22 +0200

    fix from in uppercase

--- SQL_introduction/13-change_class.sql
+++ SQL_introduction/13-change_class.sql
@@ -1,4 +1,4 @@
 -- 13 Score too low
 -- Script that removes all records with a score <=5 in second_table of hbtn_0c_0
 -- in my MySQL server.
-DELETE from second_table WHERE score <= 5;
+DELETE FROM second_table WHERE score <= 5;
