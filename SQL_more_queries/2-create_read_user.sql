4eff26ab2563fabf0574503c431f9e01eb3d024e
Mornac <imornac@gmail.com>
2025-06-24 18:26:43 +0200

    Add: USE database line 9

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -6,6 +6,7 @@
 -- script should not fail if the user user_0d_2 already exists
 DROP DATABASE IF EXISTS hbtn_0d_2;
 CREATE DATABASE hbtn_0d_2;
+USE hbtn_0d_2;
 DROP USER IF EXISTS user_0d_2@'localhost';
 CREATE USER user_0d_2@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO `user_0d_2`@'localhost';
