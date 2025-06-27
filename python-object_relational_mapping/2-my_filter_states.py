ca98665fda7c14ac339486d8f98745e1d06a86fd
Mornac <imornac@gmail.com>
2025-06-27 12:33:15 +0200

    try with import pymysql

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -3,8 +3,8 @@
 Module that takes in an argument and displays all values
 in the states table of hbtn_0e_0_usa where name matches the argument.
 """
-# import pymysql
-# pymysql.install_as_MySQLdb()
+import pymysql
+pymysql.install_as_MySQLdb()
 import MySQLdb
 import sys
 
@@ -34,8 +34,8 @@ def main():
         "SELECT * FROM states WHERE name = %s ORDER BY id ASC",
         (searched_state,)
     )
-    roows = cur.fetchall()
-    for row in roows:
+    query_roows = cur.fetchall()
+    for row in query_roows:
         print(row)
     cur.close()
     conn.close()
