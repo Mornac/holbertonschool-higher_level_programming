fe5098c15099cec0997c06f3ac0524d9ac00984a
Mornac <imornac@gmail.com>
2025-06-27 10:47:51 +0200

    try to import MySQLdb

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,38 +3,29 @@
 Module that lists all states from the database hbtn_0e_0_usa
 """
 import sys
-import pymysql
-pymysql.install_as_MySQLdb()
+import MySQLdb
 
 def main():
-    """
-    Function that lists all states
-    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
+    )
 
-
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-
-conn = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=username,
-    passwd=password,
-    db=database,
-    charset="utf8"
-)
-"""
-Create a cursor, execute the query and print results
-"""
-
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
 
 if __name__ == "__main__":
     main()
