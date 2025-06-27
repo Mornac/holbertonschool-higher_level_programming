3ebd2f89a31048fcceb51d7558e87cc95bdace6b
Mornac <imornac@gmail.com>
2025-06-27 10:04:22 +0200

    lists all states from the database hbtn_0e_0_usa with mysql username, mysql password and database name

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Module that lists all states from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+def main():
+    """
+    Function that lists all states
+    """
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+conn = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=username,
+    passwd=password,
+    db=database,
+    charset="utf8"
+)
+"""
+Create a cursor, execute the query and print results
+"""
+
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
+
+if __name__ == "__main__":
+    main()
