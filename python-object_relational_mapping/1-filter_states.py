5e578241f23d48e6a3a575e1209489edee0ef733
Mornac <imornac@gmail.com>
2025-06-27 11:30:08 +0200

    lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -3,23 +3,36 @@
 Lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa
 """
+import pymysql
+pymysql.install_as_MySQLdb()
 import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(
+def main():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    conn = MySQLdb.connect(
         host="localhost",
-        user=sys.argv[1]
-        passwd=sys.argv[2]
-        db=sys.argv[3]
-        port=3306
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
     )
 
-    cur = db.cursor()
-    cur.execute("SELECT name FROM states WHERE name = N% ORDER BY id ASC")
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    """)
     query_roows = cur.fetchall()
     for row in query_roows:
         print(row)
     cur.close()
-    db.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
