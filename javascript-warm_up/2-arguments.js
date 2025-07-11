ca13201327591bc3ab355315f49a493a85d3d117
Mornac <imornac@gmail.com>
2025-07-11 15:18:00 +0200

    change the argv of the if condition

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const string = process.argv.length;
-if (string === 2) {
+if (argv.length < 3) {
     console.log('No argument');
-} else if (string === 3) {
+} else if (argv.length === 3) {
     console.log('Argument found');
 } else {
     console.log('Arguments found');
