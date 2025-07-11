8d8875897b953f04b72b85e7c9668d2aecfb65db
Mornac <imornac@gmail.com>
2025-07-11 16:06:07 +0200

    register file 8 in file 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
-const size = parseInt(process.argv[2]);
-let line = '';
-let j;
+const {argv} = require('node:process');
+const x = parseInt(argv[2]);
 let i;
-
-if (isNaN(size) {
-  console.log('Missing size');
-} else if (size > 0) {
-  for (j = 0; j < size; j++) {
-    line += 'X';
-  }
-  for (i = 0; j < size; i++) {
-    console.log(line);
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (i = 0; i < x; i++) {
+    console.log('C is fun');
   }
 }
