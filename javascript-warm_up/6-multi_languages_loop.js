76f2d4ce86fb2d273a7f70f958a399cba460cc34
Mornac <imornac@gmail.com>
2025-07-11 15:52:36 +0200

    Add let in the for

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
-const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let i;
-for (i = 0; i < list[i]; i++) {
+const list = [
+    "C is fun", 
+    "Python is cool", 
+    "JavaScript is amazing"
+];
+
+for (let i = 0; i < list.length; i++) {
   console.log(list[i]);
 }
