b416d53a923e204127b18141f5ac109280f1aac3
Mornac <imornac@gmail.com>
2025-07-15 10:01:05 +0200

    Delete the DOCTYPE html and lang in the 2 first lines

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,6 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    document.querySelector("HEADER").style.color = "#FF0000";
-</head>
+document.querySelector("HEADER").style.color = "#FF0000";
