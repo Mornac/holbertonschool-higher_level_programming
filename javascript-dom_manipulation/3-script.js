37091281d479b64a199b89649079215a1379f7f8
Mornac <imornac@gmail.com>
2025-07-16 14:20:47 +0200

    Add the querySelector to the 1st line

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,14 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.querySelector('#toggle_header').addEventListener('DOMContentLoaded', () => {
   const header = document.querySelector('header');
-  const toggle = document.getElementById('red_header');
-
-  toggle.addEventListener('click', () => {
   if (header.classList.contains('red')) {
-    header.classList.remove('red');
-    header.classList.add('green');
+    header.classList.replace('red', 'green');
   } else {
-    header.classList.remove('green');
-    header.classList.add('red');
+    header.classList.replace('green', 'red');
   }
-  });
 });
