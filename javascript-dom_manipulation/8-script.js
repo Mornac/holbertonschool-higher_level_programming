2c6076c49d128967fbe4273b0eb6511cbaf68077
Mornac <imornac@gmail.com>
2025-07-16 14:31:11 +0200

    Use document.querySelector line 5

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -2,10 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
     .then(response => response.json())
     .then(data => {
-      const helloDiv = document.getElementById('hello');
-      helloDiv.textContent = data.hello;
-  });
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+      document.querySelector('#hello').textContent = data.hello;
+    })
+    .catch(error => console.error('Error:', error));
 });
