3f33bcfbb73a1cd4a00d1eb9851c11132951b6f5
Mornac <imornac@gmail.com>
2025-07-17 09:08:37 +0200

    Add 1st line addEventListeneer

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -1,8 +1,11 @@
-  fetch('https://swapi-api.hbtn.io/api/films/?format=json')
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      document.querySelector('#character').textContent = data.name;
-    })
-    .catch(error => console.error('Error:', error));
+  document.addEventListener('DOMContentLoaded', () => {
+    fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        document.querySelector('#character').textContent = data.name;
+      })
+      .catch(error => console.error('Error:', error));
+  });
+
