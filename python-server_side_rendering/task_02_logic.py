41183863e4b60dde86c39352b33109b83a7760e4
Mornac <imornac@gmail.com>
2025-07-18 09:45:47 +0000

    Import json in the file task 02

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
@@ -18,8 +19,9 @@ def contact():
 def items():
     with open('items.json', 'r') as file:
         data = json.load(file)
-        list = data.get('items', [])
-    return render_template ('items.html')
+        items_list = data.get('items', [])
+    return render_template ('items.html', items=items_list)
 
-if __name__ == ' __main__':
+
+if __name__ == '__main__':
     app.run(debug=True, port=5000)
